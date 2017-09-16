90d2bcb25bfc6895d2bd1060f1788c76bb0cc460
Tim Underhay <15734900+KensingtonTech@users.noreply.github.com>
2017-09-16 10:14:51 -0600

    Fixes to atomic-install.sh

--- atomic-install.sh
+++ atomic-install.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
-CERTDIR=$HOST/etc/kentech/221b/certificates
-DATADIR=$HOST/var/kentech/221b
-LOGDIR=$HOST/var/log/nginx
+CERTDIR=/etc/kentech/221b/certificates
+DATADIR=/var/kentech/221b
+LOGDIR=/var/log/nginx
 
-if [ ! -d $CERTDIR ]; then
-	mkdir -p $CERTDIR
+if [ ! -d ${HOST}${CERTDIR} ]; then
+	mkdir -p ${HOST}${CERTDIR}
 fi
 
-if [ ! -d $DATDIR ]; then
-  mkdir -p $DATADIR
+if [ ! -d ${HOST}${DATDIR} ]; then
+  mkdir -p ${HOST}${DATDIR}
 fi
 
-if [ ! -d $LOGDIR ]; then
-  mkdir -p $LOGDIR
+if [ ! -d ${HOST}${LOGDIR} ]; then
+  mkdir -p ${HOST}${LOGDIR}
 fi
 
-if [[ -f $CERTDIR/221b.key && ! -f $CERTDIR/221b.pem ]]; then
-  echo "Missing $CERTDIR/221b.pem.  Renaming $CERTDIR/221b.key to 221b.key.old"
-  mv -f $CERTDIR/221b.key $CERTDIR/221b.key.old
+if [[ -f ${HOST}${CERTDIR}/221b.key && ! -f ${HOST}${CERTDIR}/221b.pem ]]; then
+  echo "Missing ${CERTDIR}/221b.pem.  Renaming $CERTDIR/221b.key to 221b.key.old"
+  mv -f ${HOST}${CERTDIR}/221b.key ${HOST}${CERTDIR}/221b.key.old
 fi
 
-if [[ ! -f $CERTDIR/221b.key && -f $CERTDIR/221b.pem ]]; then
+if [[ ! -f ${HOST}${CERTDIR}/221b.key && -f ${HOST}${CERTDIR}/221b.pem ]]; then
   echo "Missing $CERTDIR/221b.key.  Renaming $CERTDIR/221b.pem to 221b.pem.old"
-  mv -f $CERTDIR/221b.pem $CERTDIR/221b.pem.old
+  mv -f ${HOST}${CERTDIR}/221b.pem ${HOST}${CERTDIR}/221b.pem.old
 fi
 
-if [[ ! -f $CERTDIR/221b.key || ! -f $CERTDIR/221b.pem ]]; then
+if [[ ! -f ${HOST}${CERTDIR}/221b.key || ! -f ${HOST}${CERTDIR}/221b.pem ]]; then
+  chroot $HOST
   echo "Generating new SSL keypair"
   openssl genrsa -out $CERTDIR/221b.key 2048
   openssl req -new -sha256 -key $CERTDIR/221b.key -out /tmp/tmp.csr -subj "/C=US/ST=Colorado/L=Denver/O=Kensington Technology Associates, Limited/CN=localhost/emailAddress=info@knowledgekta.com"
   openssl x509 -req -days 3650 -in /tmp/tmp.csr -signkey $CERTDIR/221b.key -out $CERTDIR/221b.pem
+  exit
 fi
 
 #copy systemd unit file to host OS
-cp -f /usr/lib/systemd/system/221b-nginx.service $HOST/usr/lib/systemd/system
+cp -f /usr/lib/systemd/system/221b-nginx.service $HOST/etc/systemd/system
 
 #load our systemd unit file
 chroot $HOST
